8e51208c0fda900a3599a8b28843063c78b51676
cclauss <cclauss@me.com>
2019-02-06 15:42:48 +0100

    convert_to_unicode() is compatible with six 1.11.0
    six_ensure_text is copied from https://github.com/benjaminp/six/blob/master/six.py#L890-L909

--- tools/lowercase_and_remove_accent.py
+++ tools/lowercase_and_remove_accent.py
@@ -14,7 +14,16 @@ def convert_to_unicode(text):
     """
     Converts `text` to Unicode (if it's not already), assuming UTF-8 input.
     """
-    return six.ensure_text(text, encoding="utf-8", errors="ignore")
+    # six_ensure_text is copied from https://github.com/benjaminp/six
+    def six_ensure_text(s, encoding='utf-8', errors='strict'):
+        if isinstance(s, six.binary_type):
+            return s.decode(encoding, errors)
+        elif isinstance(s, six.text_type):
+            return s
+        else:
+            raise TypeError("not expecting type '%s'" % type(s))
+
+    return six_ensure_text(text, encoding="utf-8", errors="ignore")
 
 
 def run_strip_accents(text):
